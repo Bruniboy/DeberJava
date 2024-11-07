f78040b3c9119ba90d615a3e5a888aac4a1e502c
Bruniboy <157723931+Bruniboy@users.noreply.github.com>
2024-11-07 10:11:43 -0500

    Ejercicio3.java

--- Ejercicio3.java
+++ Ejercicio3.java
@@ -4,18 +4,26 @@ import javax.swing.*;
 
 public class Ejercicio3 {
     public static void main(String[] args) {
-        int motor=Integer.parseInt(JOptionPane.showInputDialog("Ingrese el numero de motor"));
-        int ventanas=Integer.parseInt(JOptionPane.showInputDialog("Cuantas ventanas tiene el vehiculo"));
-        int puertas=Integer.parseInt(JOptionPane.showInputDialog("Cuantas puertas tiene el vehiculo"));
-        String marca=JOptionPane.showInputDialog("Cual es la marca del vehiculo");
-        String modelo=JOptionPane.showInputDialog("Cual es el modelo del vehiculo");
-        float kinicial=Float.parseFloat(JOptionPane.showInputDialog("Cual es el kilometraje Inicial"));
-        float kfinal=Float.parseFloat(JOptionPane.showInputDialog("Cual es el kilometraje final"));
-        float ktotal=kfinal-kinicial;
-        JOptionPane.showMessageDialog(null, "Numero de motor: "+motor +"\nEl número de ventanas es:"+ ventanas+ "\nLa cantidad de puertas: "+puertas+
-                "\nMarca: "+marca+"\nModelo: "+ modelo+"\nKilometraje Inicial: "+ kinicial+"\nKilometraje Final: "+kfinal
-                  +"\nKilometraje recorrido: "+ktotal);
+        int motor = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el numero de motor"));
+        int ventanas = Integer.parseInt(JOptionPane.showInputDialog("Cuantas ventanas tiene el vehiculo"));
+        int puertas = Integer.parseInt(JOptionPane.showInputDialog("Cuantas puertas tiene el vehiculo"));
+        String marca = JOptionPane.showInputDialog("Cual es la marca del vehiculo");
+        String modelo = JOptionPane.showInputDialog("Cual es el modelo del vehiculo");
+        float kinicial = Float.parseFloat(JOptionPane.showInputDialog("Cual es el kilometraje Inicial"));
+        float kfinal = Float.parseFloat(JOptionPane.showInputDialog("Cual es el kilometraje final"));
 
-
+        if (kfinal < kinicial) {
+            JOptionPane.showMessageDialog(null, "Error: El kilometraje final no puede ser menor que el inicial.", "Error", JOptionPane.ERROR_MESSAGE);
+        } else {
+            float ktotal = kfinal - kinicial;
+            JOptionPane.showMessageDialog(null, "Numero de motor: " + motor +
+                    "\nEl número de ventanas es: " + ventanas +
+                    "\nLa cantidad de puertas: " + puertas +
+                    "\nMarca: " + marca +
+                    "\nModelo: " + modelo +
+                    "\nKilometraje Inicial: " + kinicial +
+                    "\nKilometraje Final: " + kfinal +
+                    "\nKilometraje recorrido: " + ktotal);
+        }
     }
 }
